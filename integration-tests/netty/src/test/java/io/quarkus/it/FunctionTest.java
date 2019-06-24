86309483f4c11f10eb347df50fd7681ed8330d13
Bill Burke <bburke@redhat.com>
2019-06-24 16:33:04 -0400

    stuff

---
+++ integration-tests/netty/src/test/java/io/quarkus/it/FunctionTest.java
@@ -0,0 +1,13 @@
+package io.quarkus.it;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class FunctionTest {
+    @Test
+    public void testNetty() throws Exception {
+    }
+
+}
