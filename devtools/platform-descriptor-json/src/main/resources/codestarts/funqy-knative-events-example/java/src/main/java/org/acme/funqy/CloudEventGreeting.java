1d0b6e0bcc2bf5331883514a7e7cf9c27484d07a
Bill Burke <bburke@redhat.com>
2020-08-04 16:13:19 -0400

    lambda codestart
    lambda finish
    
    fix amazon-lambda-http archetype
    
    azure functions codestart
    
    funqy codestarts
    
    codestart supports flags
    
    codestart doc
    
    codestart finished?

---
+++ devtools/platform-descriptor-json/src/main/resources/codestarts/funqy-knative-events-example/java/src/main/java/org/acme/funqy/CloudEventGreeting.java
@@ -0,0 +1,14 @@
+package org.acme.funqy;
+
+import io.quarkus.funqy.Context;
+import io.quarkus.funqy.Funq;
+import org.jboss.logging.Logger;
+
+public class CloudEventGreeting {
+    private static final Logger log = Logger.getLogger(CloudEventGreeting.class);
+
+    @Funq
+    public void myCloudEventGreeting(Person input) {
+        log.info("Hello " + input.getName());
+    }
+}
