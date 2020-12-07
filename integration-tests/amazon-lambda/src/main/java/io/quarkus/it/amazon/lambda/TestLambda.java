79860e073bf32eba27be794ff37c0e440dd29b91
Bill Burke <bburke@redhat.com>
2020-12-07 14:33:35 -0500

    lambda @ConfigProperty test

--- integration-tests/amazon-lambda/src/main/java/io/quarkus/it/amazon/lambda/TestLambda.java
+++ integration-tests/amazon-lambda/src/main/java/io/quarkus/it/amazon/lambda/TestLambda.java
@@ -1,20 +1,29 @@
 package io.quarkus.it.amazon.lambda;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
+@ApplicationScoped
 @Named("test")
 public class TestLambda implements RequestHandler<InputObject, OutputObject> {
 
     @Inject
     ProcessingService service;
 
+    @ConfigProperty(name = "prop.name")
+    String propName;
+
     @Count
     @Override
     public OutputObject handleRequest(InputObject input, Context context) {
+        if (propName == null || !propName.equals("hello"))
+            throw new RuntimeException("config property not set");
         return service.proces(input).setRequestId(context.getAwsRequestId());
     }
 }
